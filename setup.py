dd5b007ce09f81361b6cb1e91a037cf054f6e647
mateuscmtropical <mateuscmtropical@gmail.com>
2026-09-15 19:56:13 -0300

    fix: set license metadata in setup.py used by the PyPI release workflow

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     author="Infisical",
     author_email="support@infisical.com",
     url="https://github.com/Infisical/python-sdk-official",
+    license="MIT",
     keywords=["Infisical", "Infisical API", "Infisical SDK", "SDK", "Secrets Management"],
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
